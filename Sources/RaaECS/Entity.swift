6c49e40e81391d5118d5c29cca4d75c4f4ec72ad
AlexanderARodin <rodin_aa@inbox.ru>
2022-02-20 13:46:55 +0300

    optimisation Entity.addComponent and .removeComponent

--- Sources/RaaECS/Entity.swift
+++ Sources/RaaECS/Entity.swift
@@ -26,33 +26,43 @@ extension Entity {
 		}
 	}
 	
-	func addComponentSiletnly(_ newComponent: Component) {
+	func addComponentSiletnly(_ newComponent: Component) -> Bool {
 		for component in components {
 			if type(of: newComponent) == type(of: component) {
-				return
+				return false
 			}
 		}
 		components.append(newComponent)
+		return true
 	}
-	func removeComponentSiletnly<FilterType>( withType: FilterType.Type) {
+	func removeComponentSiletnly<FilterType>( withType: FilterType.Type) -> Bool {
+		var flag = false
 		components.removeAll() { component in
-			component is FilterType
+			if component is FilterType {
+				flag = true
+				return true
+			}else{
+				return false
+			}
 		}
+		return flag
 	}
 }
 
 public extension Entity {
 	
-	func addComponent(_ newComponent: Component) {
-		addComponentSiletnly(newComponent)
-		informComponents()
+	func addComponent(_ newComponent: Component ) {
+		if addComponentSiletnly(newComponent) {
+			informComponents()
+		}
 	}
-	func removeComponent<FilterType>( withType: FilterType.Type) {
-		removeComponentSiletnly(withType: withType)
-		informComponents()
+	func removeComponent<FilterType>( withType: FilterType.Type ) {
+		if removeComponentSiletnly(withType: withType) {
+			informComponents()
+		}
 	}
 	
-	func findComponent<FilterType>( withType: FilterType.Type) -> FilterType? {
+	func findComponent<FilterType>( withType: FilterType.Type ) -> FilterType? {
 		for component in components {
 			if let component = component as? FilterType {
 				return component
